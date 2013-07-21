c176509195e0fb3f096bdebde6a99978431b5e08
dechoog@gmail.com <ubuntu@ip-172-31-40-17.us-west-2.compute.internal>
2013-07-21 00:35:00 +0000

    otro intento

--- web.js
+++ web.js
@@ -1,11 +1,12 @@
 var fs = require('fs');
 var express = require('express');
 var app = express();
-var buffer = new Buffer(fs.readFileSync("index.html");
+var buffer = new Buffer(fs.readFileSync("index.html"));
 var mystring
+mystring = buffer.toString();
 app.use(express.logger());
 
-mystring = buffer.toString();
+
 
 
 app.get('/', function(request, response) {
