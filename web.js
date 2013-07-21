f01908b9a25e227a27485337b2d6a41dcf201796
dechoog@gmail.com <ubuntu@ip-172-31-40-17.us-west-2.compute.internal>
2013-07-21 00:05:30 +0000

    nuevo intento

--- web.js
+++ web.js
@@ -1,4 +1,4 @@
-require('fs');
+var fs = require('fs');
 var express = require('express');
 var app = express();
 var buffer = new Buffer(256);
@@ -10,6 +10,7 @@ app.get('/', function(request, response) {
   buffer.write(fs.readFile('~/index.html'));
   response.send(buffer.toString());
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
