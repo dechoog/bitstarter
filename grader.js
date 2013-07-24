b1cfb06435753115695fb03151690441ae50c933
dechoog@gmail.com <ubuntu@ip-172-31-40-17.us-west-2.compute.internal>
2013-07-24 04:48:16 +0000

    version final

--- grader.js
+++ grader.js
@@ -1,19 +1,52 @@
 #!/usr/bin/env node
+/*
+Automatically grade files for the presence of specified HTML tags/attributes.
+Uses commander.js and cheerio. Teaches command line application development
+and basic DOM parsing.
+
+References:
+
+ + cheerio
+   - https://github.com/MatthewMueller/cheerio
+   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
+   - http://maxogden.com/scraping-with-node.html
+
+ + commander.js
+   - https://github.com/visionmedia/commander.js
+   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
+
+ + JSON
+   - http://en.wikipedia.org/wiki/JSON
+   - https://developer.mozilla.org/en-US/docs/JSON
+   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
+*/
 
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://nameless-tundra-4591.herokuapp.com/"
+var sys = require('util');
+var rest = require('restler');
+
+var funcion = function(result) {
+    if (result instanceof Error) {
+	sys.puts('Error: ' + result.message);
+	this.retry(5000);
+    } else {
+	sys.puts(result);
+    }
+};
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
 	console.log("%s does not exist. Exiting.", instr);
-	process.exit(1);
-	}
+	process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    }
     return instr;
-  };
+};
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -27,26 +60,48 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
     var checks = loadChecks(checksfile).sort();
     var out = {};
-    for (var ii in checks) {
-	var present = $(checks[ii]).lenght > 0;
+    for(var ii in checks) {
+	var present = $(checks[ii]).length > 0;
 	out[checks[ii]] = present;
-	}
+    }
     return out;
-   };
+};
+
+var checkUrlFile = function(htmlfile, checksfile) {
+    $ = cheerio.load(htmlfile);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
 
 var clone = function(fn) {
+    // Workaround for commander.js issue.
+    // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
 
 if(require.main == module) {
     program
 	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-    .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-    .parse(process.argv);
+	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+    .option('-u, --url <url>', 'Command line defined URL')
+       .parse(process.argv);
+    if (program.url) {
+	rest.get(program.url).on('complete', function(result) {
+	    var checkJson = checkUrlFile(result, program.checks);
+	    var outJson = JSON.stringify(checkJson, null, 4);
+	    console.log(outJson);
+	});
+	}else{
+
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
-   } else {
-       exports.checkHtmlFile = checkHtmlFile;
+	}
+} else {
+    exports.checkHtmlFile = checkHtmlFile;
 }
-
